e4b2a6c847a26ea1a2ac5e76d76664e4325fbb86
Shruthi1111 <shruthishridharan95@gmail.com>
2019-10-23 02:11:46 -0600

    part3 for lab6

--- part3.py
+++ part3.py
@@ -41,7 +41,7 @@ def create_instance(compute, project, zone, name, bucket):
     machine_type = "zones/%s/machineTypes/f1-micro" % zone
     startup_script = open(
         os.path.join(
-            os.path.dirname(__file__), 'startup-script2.sh'), 'r').read()
+            os.path.dirname(__file__), 'startup_script_2.sh'), 'r').read()
     config = {
         'name': name,
         'machineType': machine_type,
